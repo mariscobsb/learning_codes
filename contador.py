06e12ef8e0271ea58b2ceeb0f278e4b7ec94711d
Maurício <87919191+mariscobsb@users.noreply.github.com>
2021-08-15 05:11:18 -0300

    Create contador.py

---
+++ contador.py
@@ -0,0 +1,9 @@
+###### contador by https://github.com/marcelomarques05/dio-desafio-dataproc/blob/main/contador.py
+
+import sys
+from pyspark import SparkContext, SparkConf
+if __name__ == "__main__":
+    sc = SparkContext("local","PySpark Exemplo - Desafio Dataproc")
+    words = sc.textFile("gs://{SEU_BUCKET}/livro.txt").flatMap(lambda line: line.split(" "))
+    wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b).sortBy(lambda a:a[1], ascending=False)
+    wordCounts.saveAsTextFile("gs://{SEU_BUCKET}/resultado")
