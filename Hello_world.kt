e1392e2ae2676ce44583d48a75ec568bce6c4658
Maurício <87919191+mariscobsb@users.noreply.github.com>
2021-08-22 06:08:56 -0300

    Create Hello_world.kt

---
+++ Hello_world.kt
@@ -0,0 +1,15 @@
+/*playground in https://play.kotlinlang.org/ 
+this code prints the string every word in one line*/
+
+fun main() {
+
+    val name = "Hello World!"
+
+     for(n in name){
+
+              println(n)
+
+     }
+
+
+}
